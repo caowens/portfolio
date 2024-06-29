418b1afc335424b729fa7ea7d9516a7a7ef1c29e
caowens <caowens555@yahoo.com>
2024-06-29 02:36:59 -0400

    Add 3D desktop model to hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -14,9 +14,11 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className="text-[#915eff]">Alex</span></h1>
-          <p className={`${styles.heroSubText} mt-2 text-white-100`}>I develop 3D visuals, user <br className="sm:block hidden" /> interfaces, and web applciations.</p>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>I develop 3D visuals, user <br className="sm:block hidden" /> interfaces, and web applications.</p>
         </div>
       </div>
+
+      <ComputersCanvas />
     </section>
   )
 }
