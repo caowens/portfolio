b2a01f327db7901e4beb14f791fd5435b6d77b57
caowens <caowens555@yahoo.com>
2024-07-01 20:58:27 -0400

    Add correct personal info for About section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,7 +8,7 @@ import { fadeIn, textVariant } from "../utils/motion";
 
 import { SectionWrapper } from "../hoc";
 
-const ServiceCard = ({ index, title, icon}) => {
+const ServiceCard = ({ index, title, icon }) => {
   return (
     <Tilt className="xs:w-[250px] w-full">
       <motion.div
@@ -19,18 +19,19 @@ const ServiceCard = ({ index, title, icon}) => {
           options={{
             max: 45,
             scale: 1,
-            speed: 450
+            speed: 450,
           }}
           className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
         >
           <img src={icon} alt={title} className="w-16 h-16 object-contain" />
-          <h3 className="text-white text-[20px] font-bold text-center">{title}</h3>
+          <h3 className="text-white text-[20px] font-bold text-center">
+            {title}
+          </h3>
         </div>
-
       </motion.div>
     </Tilt>
   );
-}
+};
 
 const About = () => {
   return (
@@ -40,15 +41,17 @@ const About = () => {
         <h2 className={styles.sectionHeadText}>Overview.</h2>
       </motion.div>
 
-      <motion.p 
+      <motion.p
         variants={fadeIn("", "", 0.1, 1)}
         className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
       >
-        I'm a skilled software developer with experience in Typescript and
-        Javascript, and expertise in frameworks like React, Node.js, and
-        Three.js. I'm a quick learner and collaborate closely with clients to
-        create efficient, scalable, and user-friendly solutions that solve
-        real-world problems. Let's work together to bring your ideas to life!
+        I'm a skilled software developer with experience in Javascript, Python,
+        Swift, C# and expertise in frameworks like React and sNode.js.
+        I'm a recent graduate with a Bachelor's of Science in Computer Science
+        degree from Fisk University. I'm a quick learner and collaborate closely
+        with clients to create efficient, scalable, and user-friendly solutions
+        that solve real-world problems. Let's work together to bring your ideas
+        to life!
       </motion.p>
 
       <div className="mt-20 flex flex-wrap gap-10">
@@ -60,4 +63,4 @@ const About = () => {
   );
 };
 
-export default SectionWrapper(About, "about")
+export default SectionWrapper(About, "about");
